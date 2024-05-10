765851ea54ad4580271f59dc0ddbed2d6a2ce731
raajaaarkp <raja1503gaming@gmail.com>
2024-05-10 20:51:06 +0700

    Add files via upload

---
+++ synch.js
@@ -0,0 +1,10 @@
+function synchronousTask() {
+    console.log("Langkah 1");
+    console.log("Langkah 2");
+    console.log("Langkah 3");
+  }
+  
+  console.log("Proses dimulai");
+  synchronousTask();
+  console.log("Proses selesai");
+  
